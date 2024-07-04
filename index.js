ff47dca2b2e659038bfa89d5f421f3e45e2ab25b
Macbook Jo <macbook@MacBook-Air-Jo.local>
2024-07-04 17:52:34 +0200

    resolve log styles.css not find in directory

--- index.js
+++ index.js
@@ -71,15 +71,15 @@ app.use(session({
 //   }
 // }));
 
-app.use(express.static(path.join(__dirname, 'public'), {
-  setHeaders: (res, path, stat) => {
-    if (path.endsWith('.css')) {
-      res.setHeader('Content-Type', 'text/css');
-    }
-  }
-}));
+// app.use(express.static(path.join(__dirname, 'public'), {
+//   setHeaders: (res, path, stat) => {
+//     if (path.endsWith('.css')) {
+//       res.setHeader('Content-Type', 'text/css');
+//     }
+//   }
+// }));
 
-//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
